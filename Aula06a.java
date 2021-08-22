e6d4395edb0478a96794a339f0b1eaf43f4aff14
Pedro Franco <80595514+p3dr0fr4nc0@users.noreply.github.com>
2021-08-22 16:03:27 -0300

    Add files via upload

---
+++ Aula06a.java
@@ -0,0 +1,11 @@
+public class Aula06a {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+    }
+}
